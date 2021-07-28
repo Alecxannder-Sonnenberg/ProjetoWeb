b485a1b0f6f868fd498d2c4f15b93092b8efb948
Alecxannder-Sonnenberg <alecxannder.jsbf@usp.br>
2021-07-28 20:28:10 -0300

    Delete Node/node-start directory

--- Node/node-start/src/config.js
+++
@@ -1,8 +0,0 @@
-global.SALT_KEY = 'ITHINKTHISCANBEANYTHINGIWANT';
-global.EMAIL_TMPL = '<strong>{0}</strong>';
-
-module.exports = {
-    connectionString: 'mongodb+srv://WebGroup:Password@webclass.juiuh.mongodb.net/WebClass?retryWrites=true&w=majority',
-    sendgridKey: 'TBD',
-    containerConnectionString: 'TBD'
-}
